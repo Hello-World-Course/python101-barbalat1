9221fe70a2288700f826f1bf1d57ffc076a6e500
sbarbala <barbalat101@gmail.com>
2025-03-17 17:02:53 +0200

    Minesweeper2

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,5 +1,5 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
 name = input("Hello, whats your name\n ")
 board_size = int(input(f"{name}, please choose board size\n "))
-number_of_mines = int(input(f"{name}, for board size board size {board_size}, choose number of mines to allocate\n"))
+number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate\n"))
 print(f"{name}, the board size is: {board_size}, number of mines is:{number_of_mines}. ENJOY!")
