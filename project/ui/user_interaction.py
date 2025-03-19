cade9233e8c37bd6a9766d82133092d07c6968a3
sbarbala <barbalat101@gmail.com>
2025-03-19 13:33:32 +0200

    lass0n8_10

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -3,13 +3,13 @@ name = input("Hello, whats your name")
 num_char_name= len(name)
 if num_char_name < 2 :
     print('Your name is too short')
-    name=None
+    name="None"
 
 user_input = input(f"{name}, please choose board size")
 
 if user_input.isdigit():  # בדיקה שהקלט מספרי
     board_size = int(user_input)
-    if not (0 <= board_size <= 26):  # עכשיו מקבל בין 0 ל-26
+    if not (1 <= board_size <= 25):  # עכשיו מקבל בין 0 ל-26
         print(f"{name}, you have entered illegal board size")
         board_size = None
 else:
@@ -21,7 +21,7 @@ if board_size is not None:  # רק אם גודל הלוח חוקי
 
     if user_input1.isdigit():
         number_of_mines = int(user_input1)
-        max_mines = (board_size * board_size) // 2  # חצי מגודל הלוח
+        max_mines = (board_size * board_size) / 2  # חצי מגודל הלוח
 
         if not (1 <= number_of_mines <= max_mines):  # מספר מוקשים חייב להיות בטווח
             print(f"{name}, you have entered illegal number of mines")
